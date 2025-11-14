d4fa7c552c76c57db689bace560813911426030f
TheJoshJ <joshua.jones@shipt.com>
2025-11-14 11:03:37 -0800

    mode toggle & layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from "./mode-toggle"
+
+export const Navbar = () => {
+  return (
+    <div className="w-full flex flex-row justify-end">
+      <ModeToggle />
+    </div>
+  )
+}
