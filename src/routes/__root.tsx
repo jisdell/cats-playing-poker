07cc35d605df73c9b4a65d54a4ffa736c88207d6
Hunter Wesson <hunter.wesson@shipt.com>
2026-01-30 14:17:12 -0600

    Add router devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,6 +4,7 @@ import type { usePokerApi } from '@/hooks/usePokerApi'
 import { ScrollArea } from '@radix-ui/react-scroll-area'
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { Toaster } from 'sonner'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRouteWithContext<
   ReturnType<typeof usePokerApi>
@@ -13,21 +14,24 @@ export const Route = createRootRouteWithContext<
 
 function RootComponent() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="h-screen w-screen flex flex-col bg-background text-foreground">
-        <header className="h-[60px] bg-background text-foreground py-2 border-b border-muted">
-          <Navbar />
-        </header>
+    <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div className="h-screen w-screen flex flex-col bg-background text-foreground">
+          <header className="h-[60px] bg-background text-foreground py-2 border-b border-muted">
+            <Navbar />
+          </header>
 
-        <main className="flex flex-col">
-          <ScrollArea className="overflow-x-auto">
-            <div className="h-[calc(100vh-60px)] flex">
-              <Outlet />
-            </div>
-          </ScrollArea>
-          <Toaster position="bottom-left" richColors />
-        </main>
-      </div>
-    </ThemeProvider>
+          <main className="flex flex-col">
+            <ScrollArea className="overflow-x-auto">
+              <div className="h-[calc(100vh-60px)] flex">
+                <Outlet />
+              </div>
+            </ScrollArea>
+            <Toaster position="bottom-left" richColors />
+          </main>
+        </div>
+      </ThemeProvider>
+      <TanStackRouterDevtools />
+    </>
   )
 }
